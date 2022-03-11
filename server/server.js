8f31822a290db7c1ba237d107b2dc72625ae0cfa
khaoula <khaoulajabour4@gmail.com>
2022-03-11 01:42:49 -0500

    use jwt to authorize users  + create mutations

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {ApolloServer}= require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
@@ -11,8 +12,8 @@ const startServer = async ()=>{
     // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
-    resolvers
-   // context: authMiddleware
+    resolvers,
+    context: authMiddleware
   });
 
   //start apollo server
