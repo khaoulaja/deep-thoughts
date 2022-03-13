7713ed0c4f71e7f4282ea194d1055740c2df0e06
khaoula <khaoulajabour4@gmail.com>
2022-03-13 03:42:02 -0400

    add frontend user authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Header from './components/Header';
@@ -15,8 +16,19 @@ import Signup from './pages/Signup';
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
+//With  setContext function, we can create essentially a middleware function that will retrieve the token for us and combine it with the existing httpLink.
+const authLink = setContext((_, { headers }) => { //underscore _ to serve as a placeholder for the first parameter.
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+//we need to combine the authLink and httpLink objects so that every request retrieves the token and sets the request headers before making the request to the API. 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 function App() {
