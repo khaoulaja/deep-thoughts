cd20213e4f577396ca79427bffc6d97bc3eb8c13
khaoula <khaoulajabour4@gmail.com>
2022-03-13 06:35:34 -0400

    touch ups

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -31,13 +31,18 @@ const ThoughtForm = () => {
             } catch ( e ){
                 console.error(e);
             }
-            
-            // update me object's cache, appending new thought to the end of the array
-            const { me } = cache.readQuery({ query: QUERY_ME });
-            cache.writeQuery({
-            query: QUERY_ME,
-            data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
-            });   
+           
+            try{
+
+                // update me object's cache, appending new thought to the end of the array
+                const { me } = cache.readQuery({ query: QUERY_ME });
+                cache.writeQuery({
+                    query: QUERY_ME,
+                    data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+                });   
+            } catch (e){
+                console.error(e);
+            }
         }
     });
     
